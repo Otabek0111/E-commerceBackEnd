3ac49c398aad0e3f186372346ea650a70b97add8
Otabek Islomjonov <134247761+Otabek0111@users.noreply.github.com>
2023-10-23 16:21:59 -0400

    added schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,37 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- USE DATABASE
+USE ecommerce_db;
+
+CREATE TABLE categories (
+    id INT NOT NULL AUTO_INCREMENT,
+    category_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id) 
+);
+
+CREATE TABLE products (
+    id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(30) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock INT DEFAULT 10,
+    category_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (category_id) REFERENCES categories (id)
+);
+
+CREATE TABLE tags (
+    id INT NOT NULL AUTO_INCREMENT,
+    tag_name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE product_tags (
+    id INT NOT NULL AUTO_INCREMENT,
+    product_id INT,
+    tag_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (product_id) REFERENCES products (id),
+    FOREIGN KEY (tag_id) REFERENCES tags (id)
+);
+
